16aa28bfcb16945a19664ca9eee0048a43866040
Dylan Tran <dylanbt928@gmail.com>
2025-04-09 00:12:47 -0700

    create basic sfml window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
+#include <SFML/Graphics.hpp>
+
 #include "game.cpp"
 
 int main()
 {
-    Game blackjack(10);
-    blackjack.playGame();
+    sf::RenderWindow window(sf::VideoMode({1200, 800}), "Blackjack");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Escape) ||
+                event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        window.display();
+    }
 
     return 0;
 }
