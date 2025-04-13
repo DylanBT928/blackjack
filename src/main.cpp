d0de42926cfe1a6bf582d4d3ccc0f790ef1facf3
Dylan Tran <dylanbt928@gmail.com>
2025-04-12 22:25:34 -0700

    created table.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
-#include <SFML/Graphics.hpp>
-
 #include "game.cpp"
+#include "table.cpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({1200, 800}), "Blackjack",
-                            sf::Style::Titlebar | sf::Style::Close);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Escape) ||
-                event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        window.clear(sf::Color(20, 60, 50));
-
-        window.display();
-    }
+    Table table;
+    table.setupTable();
 
     return 0;
 }
---
+++ src/table.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+class Table
+{
+   public:
+    explicit Table() = default;
+
+    void setupTable()
+    {
+        sf::RenderWindow window(sf::VideoMode({1200, 800}), "Blackjack",
+                                sf::Style::Titlebar | sf::Style::Close);
+
+        while (window.isOpen())
+        {
+            while (const std::optional event = window.pollEvent())
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Escape) ||
+                    event->is<sf::Event::Closed>())
+                {
+                    window.close();
+                }
+            }
+
+            window.clear(sf::Color(20, 60, 50));
+
+            window.display();
+        }
+    }
+};
