b23e3fa1ba2322c8cda96b6015a4f8941c627e7f
Dylan Tran <dylanbt928@gmail.com>
2025-06-01 02:44:44 -0700

    draw buttons

--- src/table.cpp
+++ src/table.cpp
@@ -5,7 +5,7 @@ Table::Table()
     window = sf::RenderWindow(sf::VideoMode({1200, 800}), "Blackjack",
                               sf::Style::Titlebar | sf::Style::Close);
 
-    if (!font.openFromFile("assets/Arial.tff"))
+    if (!font.openFromFile("assets/Arial.ttf"))
         std::cerr << "Failed to load font\n";
 
     while (window.isOpen())
@@ -44,4 +44,42 @@ void Table::drawEmptyCards()
     }
 }
 
-void Table::drawButtons() { ; }
+void Table::drawButtons()
+{
+    sf::RectangleShape hitButton({378.0f, 81.0f});
+    hitButton.setFillColor(sf::Color(171, 230, 234));
+    hitButton.setPosition({73.0f, 369.0f});
+    window.draw(hitButton);
+
+    sf::Text hitLabel(font);
+    hitLabel.setString("Hit");
+    hitLabel.setCharacterSize(48);
+    hitLabel.setFillColor(sf::Color::Black);
+
+    auto textBounds = hitLabel.getLocalBounds();
+    hitLabel.setOrigin({textBounds.position.x + textBounds.size.x / 2.f,
+                        textBounds.position.y + textBounds.size.y / 2.f});
+
+    hitLabel.setPosition(hitButton.getPosition() + hitButton.getSize() / 2.f);
+
+    window.draw(hitLabel);
+
+    sf::RectangleShape standButton({378.0f, 81.0f});
+    standButton.setFillColor(sf::Color(254, 185, 170));
+    standButton.setPosition({748.0f, 369.0f});
+    window.draw(standButton);
+
+    sf::Text standLabel(font);
+    standLabel.setString("Stand");
+    standLabel.setCharacterSize(48);
+    standLabel.setFillColor(sf::Color::Black);
+
+    textBounds = standLabel.getLocalBounds();
+    standLabel.setOrigin({textBounds.position.x + textBounds.size.x / 2.f,
+                          textBounds.position.y + textBounds.size.y / 2.f});
+
+    standLabel.setPosition(standButton.getPosition() +
+                           standButton.getSize() / 2.f);
+
+    window.draw(standLabel);
+}
